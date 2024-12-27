da57d1e26657ced9a2f66a0c128f1e00fe778426
林雄军 <416596401@qq.com>
2024-12-27 17:37:43 +0800

    新增配置文件

---
+++ config.js
@@ -0,0 +1,12 @@
+
+// 接口地址
+export const basehost = "http://47.119.182.138:88/";
+
+// 腾讯地图key
+export const mapKey = "RWEBZ-3S3CI-VMZGO-5UDDA-JCXYE-MTBWZ";
+
+// 腾讯IM 
+export const imKey = '1600067113';
+
+// 人脸识别回调web地址
+export const faceRedirectUrl = 'https://login.derucci-smart.com';
